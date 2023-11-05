9ec6ecb845ae62c46549de55b8a325aac6b54108
Maria Aurellia <mariaadannies26@gmail.com>
2023-11-06 06:13:13 +0700

    dataset

--- profiler.py
+++ profiler.py
@@ -21,8 +21,6 @@ def profile_algorithm_cbis(data):
 
     return result, execution_time, memory_used
 
-dataset = generate_dataset()
-
 @profile
 def profile_algorithm_rqis(data):
     start_time = time.time()
@@ -39,27 +37,25 @@ def profile_algorithm_rqis(data):
     return result, execution_time, memory_used
 
 dataset = generate_dataset()
-
-# for data in dataset:
-#     result_1, time_1, memory_1 = profile_algorithm_cbis(data[0])
-#     result_2, time_2, memory_2 = profile_algorithm_rqis(data[0])
-
-#     print("CBIS: Execution time =", time_1, "Memory used =", memory_1, "size = ", data[1], "status = ", data[2])
-#     print("RQIS: Execution time =", time_2, "Memory used =", memory_2, "size = ", data[1], "status = ", data[2])
-
+sizes = [200, 2000, 20000]
+statuses = ["sorted", "reversed", "random"]
 table_time = PrettyTable()
 table_memory = PrettyTable()
 table_time.field_names = ["Size", "Status", "CBIS", "RQIS"]
 table_memory.field_names = ["Size", "Status", "CBIS", "RQIS"]
 
-for data in dataset:
-    result_1, time_1, memory_1 = profile_algorithm_cbis(data[0])
-    result_2, time_2, memory_2 = profile_algorithm_rqis(data[0])
+for size in sizes:
+    for status in statuses:
+        # read dataset from file
+        with open("dataset_" + str(size) + "_" + status + ".txt", "r") as file:
+            data = file.read().splitlines()
+        result_1, time_1, memory_1 = profile_algorithm_cbis(data)
+        result_2, time_2, memory_2 = profile_algorithm_rqis(data)
 
-    print("CBIS: Execution time =", time_1, "Memory used =", memory_1, "size = ", data[1], "status = ", data[2])
-    print("RQIS: Execution time =", time_2, "Memory used =", memory_2, "size = ", data[1], "status = ", data[2])
-    table_time.add_row([data[1], data[2], time_1, time_2])
-    table_memory.add_row([data[1], data[2], memory_1, memory_2])
+        print("CBIS: Execution time =", time_1, "Memory used =", memory_1, "size = ", size, "status = ", status)
+        print("RQIS: Execution time =", time_2, "Memory used =", memory_2, "size = ", size, "status = ", status)
+        table_time.add_row([size, status, time_1, time_2])
+        table_memory.add_row([size, status, memory_1, memory_2])
 
 with open("output.txt", "w") as file:
     file.write("Execution Time Comparison\n")
--- random_dataset.py
+++ random_dataset.py
@@ -19,4 +19,8 @@ def generate_dataset():
             arr = generate_array(size, status)
             print(f"Array with size {size} and status {status}: {arr[:10]}... ({len(arr)} elements in total)")
             dataset.append([arr, size, status])
+            # create txt file for each dataset
+            with open(f"dataset_{size}_{status}.txt", "w") as file:
+                for element in arr:
+                    file.write(f"{element}\n")
     return dataset
